25a978c2eab486e43608fa73db8ef0fab99bfb9c
julie <julie@DESKTOP-5L3T3C6>
2023-01-23 09:53:26 -0600

    before Tues Jan 24 lab

---
+++ lab2/lab2test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sum_array(int arr[]) {
+    int sum = 0;
+    for (int i = 0; i < sizeof(arr); i++) {
+        sum = sum + arr[i];
+    }
+
+    return sum;
+}
+
+int main() {
+    int test[] = {1,2,3,4};
+    printf("Array sum from function: %d\n", sum_array(test));
+
+    return 0;
+}
