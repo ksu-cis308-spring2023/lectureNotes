b005404d1fe34c69ac2ca71c2905262537f71e25
julie <julie@DESKTOP-5L3T3C6>
2023-01-31 11:36:02 -0600

    after class Tues Jan 31

--- lab3/lab3example.c
+++ lab3/lab3example.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
+    char buffer[100];
+
+    while (fgets(buffer, 100, stdin) != NULL) {
+        //buffer is the current line from the file
+        buffer[strcspn(buffer, "\n")] = '\0';
+        printf("%s %s %s\n", argv[1], buffer, argv[2]);
+    }
 
     return 0;
 }
