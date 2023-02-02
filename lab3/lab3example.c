4f1f87e245398617794d89ec271c7644ca36f78c
julie <julie@DESKTOP-5L3T3C6>
2023-02-02 11:30:53 -0600

    after Thurs Feb 2

--- lab3/lab3example.c
+++ lab3/lab3example.c
@@ -5,6 +5,9 @@ int main(int argc, char* argv[]) {
     //read each line with fgets
 
     //print the 1st arg, then current line, then 2nd arg
+    char line[100];
+    fgets(line, 100, stdin);
+    printf("First line: %s\n", line);
 
     return 0;
 }
