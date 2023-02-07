757fda10131d17a904cef026c5807032cc6adde7
julie <julie@DESKTOP-5L3T3C6>
2023-02-07 11:39:22 -0600

    after Tues Feb 7

--- lab4/lab4example.c
+++ lab4/lab4example.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 
-void swap(int, int);
+void swap(int*, int*);
 
 void print_array(int*, int);
 
 int main() {
-    int x = 3;
-    int y = 4;
     int nums[] = {1,2,3,4,5,6,7,8,9,10};
 
-    printf("Old values: x = %d, y = %d\n", x, y);
+    //call swap to swap the 2 and the 4
+    swap(nums+1, nums+3);
 
-    //call swap
 
-    printf("New values: x = %d, y = %d\n", x, y);
+    int x = 4;
+    int y = 3;
+    swap(&x, &y);
 
     //call print_array
+    print_array(nums, 10);
 
     return 0;
 }
 
 //will this work?
-void swap(int a, int b) {
-	int temp = a;
-	a = b;
-	b = temp;
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void print_array(int* nums, int len) {
     //print array values using pointer notation
+    for (int* ptr = nums; ptr < nums + len; ptr++) {
+        printf("%d ", *ptr);
+    }
 }
