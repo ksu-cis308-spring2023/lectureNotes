023c9759c2b89cadeb1f66d6e6ffd40bdf173607
julie <julie@DESKTOP-5L3T3C6>
2023-02-22 14:16:18 -0600

    before Thurs Feb 23

--- lab6/factorial.c
+++ lab6/factorial.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int prod = 1;
+    int prod = 0;
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i < 3; i++) {
         prod = prod * i;
     }
 
+    //Should print 3!, which is 6
     printf("Product is: %d\n", prod);
 
     return 0;
