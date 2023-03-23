5c9e8d95285e6ba125a0a2bc1fcdf6d01340b976
julie <julie@DESKTOP-5L3T3C6>
2023-03-23 11:35:54 -0500

    after class Thurs March 23

--- lab8/lab8example.c
+++ lab8/lab8example.c
@@ -6,5 +6,13 @@ int main() {
     //want to create a linked list with
     //items 1-4, print it, release the memory
 
+    struct node* list = NULL;
+    for (int i = 1; i <= 4; i++) {
+        add(i, &list);
+    }
+
+    printlist(list);
+    releasemem(list);
+
     return 0;
 }
--- lab8/linkedlist.c
+++ lab8/linkedlist.c
@@ -2,15 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void add(int elem, struct node* head) {
+void add(int elem, struct node** head) {
+    if (*head == NULL) {
+        *head = malloc(sizeof(struct node));
+        (*head)->data = elem;
+        (*head)->next = NULL;
+    }
+    else {
+        struct node* temp = *head;
 
+        //loop until we get to the last node
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+
+        temp->next = malloc(sizeof(struct node));
+        temp->next->data = elem;
+        temp->next->next = NULL;
+    }
     //we will see this function has a problem
 }
 
 void printlist(struct node* head) {
-
+    struct node* temp = head;
+    while (temp != NULL) {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
 }
 
 void releasemem(struct node* head) {
-
+    struct node* temp = head;
+    while (temp != NULL) {
+        struct node *after = temp->next;
+        free(temp);
+        temp = after;
+    }
 }
