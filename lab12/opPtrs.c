65364bb7fda72ebdcec4ae341d1d5c8a2b5e2c98
julie <julie@DESKTOP-5L3T3C6>
2023-04-18 11:39:50 -0500

    after Tues April 18

--- lab12/opPtrs.c
+++ lab12/opPtrs.c
@@ -16,7 +16,7 @@ int divide(int a, int b) {
     return a / b;
 }
 
-
+typedef int (*OP) (int, int);
 
 //Define doOperations function. Takes three parameters:
     //function pointer to a function that does the operation
@@ -24,16 +24,23 @@ int divide(int a, int b) {
     //two ints to do the operation on
 //returns result of whatever operation function
 
+int doOperations(OP ptr, int x, int y) {
+    return ptr(x, y);
+}
 
 
 int main() {
     int num1 = 3;
     int num2 = 4;
 
-    //multiply num1 and num2
-    int answer;
+    OP operations[4] = {plus,minus,times,divide};
 
     
+
+    //maybe user enters 0-3 into "index"
+
+    //do operation num1 and num2
+    int answer = operations[index](num1, num2);
 
     printf("Answer: %d\n", answer);
 
