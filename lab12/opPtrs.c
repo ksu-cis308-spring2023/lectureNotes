6d81c69274c4e7794280beaf862460aa62a5a569
julie <julie@DESKTOP-5L3T3C6>
2023-04-20 11:39:54 -0500

    after Thurs Apr 20

--- lab12/opPtrs.c
+++ lab12/opPtrs.c
@@ -16,7 +16,11 @@ int divide(int a, int b) {
     return a / b;
 }
 
+typedef (int (*OP)(int, int);
 
+int doOperation(OP op, int x, int y) {
+    return op(x, y);
+}
 
 //Define doOperations function. Takes three parameters:
     //function pointer to a function that does the operation
@@ -31,10 +35,10 @@ int main() {
     int num1 = 3;
     int num2 = 4;
 
-
+    OP allops[4] = {plus, minus, times, divide};
 
     //do operation num1 and num2
-    int answer;
+    int answer = allops[userInput](num1, num2);
 
     //multiply num1 and num2 using doOperation
 
