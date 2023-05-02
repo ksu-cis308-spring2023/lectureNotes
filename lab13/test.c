edaf90e6ae785c82c92227e89d42bb0d68eea4a6
julie <julie@DESKTOP-5L3T3C6>
2023-05-02 11:32:28 -0500

    after Tues May 2

--- lab13/test.c
+++ lab13/test.c
@@ -4,15 +4,24 @@ int main() {
     int num = 4;
     void* ptr;
     //how to initialize ptr to point to num?
+    ptr = &num;
+
+    //ptr++; <-- can't do this, don't know how big
+                //each memory spot is
+
 
     //how to print num using ptr?
+    printf("%d\n", *((int*)ptr));
+
     //(need to cast to actual pointer type first)
 
 
     char* str = "hello";
     //make ptr point to hello
+    ptr = str;
 
     //use ptr to print the "h"
+    printf("%c\n", ((char*)ptr)[0]);
 
     return 0;
 }
